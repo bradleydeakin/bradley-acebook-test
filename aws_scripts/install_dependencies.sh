c9ebfc7710d60a1ddcaf774f4d40f0ecb8fa5bb8
Bradley Deakin <bradleydeakin2@gmail.com>
2022-05-20 10:25:49 +0100

    delete bucket

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # source /home/ec2-user/.bash_profile
-aws s3 cp s3://bradley-acebook-app.zip /home/ec2-user/acebook-test/bradley-acebook-app.zip
+if [ -d /home/ec2-user/acebook-test ]; then
+    rm -rf /home/ec2-user/acebook-test/
+fi
+aws s3 cp s3://bradley-acebook-app /home/ec2-user/acebook-test/bradley-acebook-app.zip
 mkdir /home/ec2-user/acebook-test
 cd /home/ec2-user/acebook-test
 npm install
