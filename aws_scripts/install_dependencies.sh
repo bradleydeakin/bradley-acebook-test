67b0ee6762040235984558588406efd7ca1b7c45
Bradley Deakin <bradleydeakin2@gmail.com>
2022-05-20 11:16:35 +0100

    update

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# source /home/ec2-user/.bash_profile
-if [ -d /home/ec2-user/acebook-test ]; then
-    rm -rf /home/ec2-user/acebook-test/
-fi
-aws s3 cp s3://bradleydeakin-acebook-test /home/ec2-user/acebook-test
+source /home/ec2-user/.bash_profile
+# if [ -d /home/ec2-user/acebook-test ]; then
+#     rm -rf /home/ec2-user/acebook-test/
+# fi
+# aws s3 cp s3://bradleydeakin-acebook-test /home/ec2-user/acebook-test
 
-mkdir /home/ec2-user/acebook-test
-cd acebook-test
+# mkdir /home/ec2-user/acebook-test
+cd /home/ec2-user/acebook-test
 npm install
