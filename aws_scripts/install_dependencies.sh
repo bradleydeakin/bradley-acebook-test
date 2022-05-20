2603e9bce67bfaf976c794e77b4272bc335b2bea
Bradley Deakin <bradleydeakin2@gmail.com>
2022-05-20 10:19:50 +0100

    role change

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # source /home/ec2-user/.bash_profile
+aws s3 cp s3://bradley-acebook-app.zip /home/ec2-user/acebook-test/bradley-acebook-app.zip
 mkdir /home/ec2-user/acebook-test
 cd /home/ec2-user/acebook-test
 npm install
