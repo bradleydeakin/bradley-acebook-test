07f02f159c5d300f3fb32e26d34df3446c7063e4
Bradley Deakin <bradleydeakin2@gmail.com>
2022-05-20 11:05:44 +0100

    update files

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -3,7 +3,7 @@
 if [ -d /home/ec2-user/acebook-test ]; then
     rm -rf /home/ec2-user/acebook-test/
 fi
-aws s3 cp s3://bradleydeakin-acebook-test download: s3://bradleydeakin-acebook-test to /home/ec2-user/acebook-test/bradleydeakin-acebook-app.zip
+aws s3 cp s3://bradleydeakin-acebook-test /home/ec2-user/acebook-test
 
 mkdir /home/ec2-user/acebook-test
 cd /home/ec2-user/acebook-test
