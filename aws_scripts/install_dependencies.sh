1dc4f946294af55d0af630d3a6e731d27c2966e1
Bradley Deakin <bradleydeakin2@gmail.com>
2022-05-20 09:42:43 +0100

    npm scripts

--- aws_scripts/install_dependencies.sh
+++ aws_scripts/install_dependencies.sh
@@ -2,6 +2,4 @@
 # source /home/ec2-user/.bash_profile
 mkdir /home/ec2-user/acebook-test
 cd /home/ec2-user/acebook-test
-sudo apt-get update
-sudo apt-get install nodejs
-sudo apt-get install npm
+npm install
